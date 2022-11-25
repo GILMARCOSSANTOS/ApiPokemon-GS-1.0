097138d5e8a7d51364845295bb4f5d365dc040b0
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-11-25 19:34:12 -0300

    Salvar

---
+++ app/src/main/java/com/example/apipokemon_20/api/adapter_pokemon/PokemonAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.apipokemon_20.api.adapter_pokemon
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apipokemon_20.R
+
+class PokemonAdapter() : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
+
+    class PokemonViewHolder(){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
